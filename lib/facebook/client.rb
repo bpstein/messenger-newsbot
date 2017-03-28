07701c7d17689987cceadcf5a0af640dbc3fa4f7
Ben Stein <bumstain@gmail.com>
2017-03-28 09:50:36 +0100

    Exchanged token for long-lived access token

--- lib/facebook/client.rb
+++ lib/facebook/client.rb
@@ -2,7 +2,7 @@ module Facebook
   class Client
 
     def initialize
-      @base_url = "http://graph.facebook.com/v2.8"
+      @base_url = "https://graph.facebook.com/v2.8"
     end
 
     def get_user(fb_id)
@@ -10,12 +10,19 @@ module Facebook
     end
 
     def exchange_token
-      
+      options = {
+        grant_type: 'fb_exchange_token',
+        client_id: ENV["FB_APP_ID"],
+        client_secret: ENV["FB_APP_SECRET"],
+        fb_exchange_token: ENV["FB_ACCESS_TOKEN"]
+      }
+
+      get("/oauth/access_token", options)
     end
 
     private
 
-    def get(path, options{})
+    def get(path, options = {})
       conn = Faraday.new(@base_url)
       resp = conn.get(path, options)
       resp.body
