91f88c4118e5d93168471415745b1c8d6cade2dd
Ben Stein <bumstain@gmail.com>
2017-03-28 09:13:40 +0100

    Parrot bot functionality works successfully

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -4,6 +4,13 @@ include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe
 
+
+
 Bot.on :message do |message|
-  puts "got your message!"
+  Bot.deliver(
+    recipient: message.sender, 
+    message: {
+      text: message.text
+    }
+  )
 end
