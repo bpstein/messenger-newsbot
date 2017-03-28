cc19b171cc25dbc758afe411a91c3851c598ea15
Ben Stein <bumstain@gmail.com>
2017-03-28 09:59:30 +0100

    Bot responds to user based on specific user Facebook data

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -7,10 +7,15 @@ Facebook::Messenger::Subscriptions.subscribe
 
 
 Bot.on :message do |message|
+
+  client = Facebook::Client.new 
+  user_data = client.get_user(message.sender["id"])
+  user_name = user_data["first_name"]
+
   Bot.deliver(
     recipient: message.sender, 
     message: {
-      text: message.text
+      text: "Hi #{user_name}!"
     }
   )
 end
