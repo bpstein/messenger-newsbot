da70e162e987434e9a30bb970e6bee9406c14978
Ben Stein <bumstain@gmail.com>
2017-03-28 10:57:10 +0100

    Created users and logs in the Brain class

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -4,18 +4,39 @@ include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe
 
-
+# message.id            
+# message.sender
+# message.seq
+# message.sent_at
+# message.text
+# message.attachments 
 
 Bot.on :message do |message|
 
-  client = Facebook::Client.new 
-  user_data = client.get_user(message.sender["id"])
-  user_name = user_data["first_name"]
+  brain = Brain.new
+  brain.set_message(message)
+  brain.start_typing
+  brain.create_log
+  brain.process_message
+  brain.stop_typing
 
-  Bot.deliver(
-    recipient: message.sender, 
-    message: {
-      text: "Hi #{user_name}!"
-    }
-  )
+  # client = Facebook::Client.new 
+  # user_data = client.get_user(message.sender["id"])
+  # user_name = user_data["first_name"]
+
+  # Bot.deliver(
+  #   recipient: message.sender, 
+  #   message: {
+  #     text: "Hi #{user_name}!"
+  #   }
+  # )
+end
+
+Bot.on :postback do |postback|
+  brain = Brain.new
+  brain.set_postback(message)
+  brain.start_typing
+  brain.create_log
+  brain.process_postback
+  brain.stop_typing
 end
